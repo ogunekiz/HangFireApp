601b6c00bbb0863b66eff0ef5d3291da93038872
Ogün EKİZ <ogunekiz@hotmail.com>
2024-10-05 14:51:51 +0300

    Hangfire configure edildi.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,37 @@
+using Hangfire;
+using HangFireApp.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+string connectionstring = builder.Configuration.GetConnectionString("SqlServer");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionstring);
+});
+
+builder.Services.AddHangfire(config =>
+{
+    config.UseSqlServerStorage(connectionstring);
+});
+
+builder.Services.AddHangfireServer();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHangfireDashboard();
 
 app.UseHttpsRedirection();
 
